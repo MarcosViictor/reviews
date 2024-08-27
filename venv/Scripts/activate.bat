9080e174055326526168c3e200b7ad495b1ea3ae
João Filho <joaodaviabc@gmail.com>
2024-08-27 01:11:05 -0300

    requirements

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\victo\OneDrive\Imagens\Documentos\projetos\pi-sistemas-distribuidos\reviews\venv
+set VIRTUAL_ENV=C:\Users\Administrator\source\repos\reviews\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
