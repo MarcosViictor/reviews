1d44b8f7323c112a828fdb8670f314e9f251aa4e
João Filho <joaodaviabc@gmail.com>
2024-09-11 16:50:48 -0300

    Organização readme, venv, base de dependências.

--- Frontend/movies/src/App.tsx
+++ Frontend/movies/src/App.tsx
@@ -1,14 +1,14 @@
 
-import './assets/styles/index.css'
+import './assets/styles/index.css';
 import { FaBeer } from "react-icons/fa";
 
 function App() {
 
   return (
     <>
-      <h1 className=" font-bold underline bg-slate-400 p-10">
+      <h1 className="font-bold underline bg-slate-400 p-10">
       <FaBeer />
-    </h1>
+      </h1>
     {/* criar rootas aqui */}
       
     </>
