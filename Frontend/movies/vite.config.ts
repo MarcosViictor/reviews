c680e945598fe06efb70cedb71290a31d5a9a44c
Luis <luiss18.code@gmail.com>
2024-09-20 17:09:06 -0300

    configuração do docker para o frontend

--- Frontend/movies/vite.config.ts
+++ Frontend/movies/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,  // Pode mudar para 3000 se preferir
+    host: '0.0.0.0',  // Isso permite que o Vite seja acessado externamente
+    watch: {
+      usePolling: true,  // Necessário no Docker para ativar o hot reload
+    },
+  },
+  base: './',  // Garante que caminhos relativos funcionem ao recarregar
 })
