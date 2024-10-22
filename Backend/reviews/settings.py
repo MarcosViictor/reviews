4a37539c8af92ee9a5d74a9451489f75dbc67ba1
Geovanaiviny <ivinylima5@gmail.com>
2024-10-22 18:44:40 -0300

    deu certo amem

--- Backend/reviews/settings.py
+++ Backend/reviews/settings.py
@@ -78,13 +78,26 @@ WSGI_APPLICATION = 'reviews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Define que está usando PostgreSQL
+        'NAME': 'reviews',           # Substitua pelo nome do seu banco de dados
+        'USER': 'postgres',                     # Substitua pelo seu nome de usuário do PostgreSQL
+        'PASSWORD': 'root6',                   # Substitua pela senha do seu banco de dados
+        'HOST': 'localhost',                       # Use 'localhost' se o banco estiver rodando localmente
+        'PORT': '5432',                            # A porta padrão do PostgreSQL é 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
