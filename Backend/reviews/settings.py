65c66babec79e31be06055a8a27c704338756e29
victor <victorfire99@gmail.com>
2024-10-23 15:07:33 -0300

    ajsute

--- Backend/reviews/settings.py
+++ Backend/reviews/settings.py
@@ -78,13 +78,19 @@ WSGI_APPLICATION = 'reviews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reviews',  # Substitua pelo nome do seu banco de dados
+        'USER': 'postgres',  # Substitua pelo seu usuário do PostgreSQL
+        'PASSWORD': 'root',  # Substitua pela sua senha
+        'HOST': 'localhost',  # Ou o endereço do servidor de banco de dados
+        'PORT': '5432',  # A porta padrão do PostgreSQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
