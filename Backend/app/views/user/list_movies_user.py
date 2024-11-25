393075b08411c9696b283986e2e6f92a3cf60b25
victor <victorfire99@gmail.com>
2024-11-25 18:05:58 -0300

    ajuste na exibição de filmes realizados pelo usuario

---
+++ Backend/app/views/user/list_movies_user.py
@@ -0,0 +1,5 @@
+from requests import Response
+from rest_framework import generics
+from rest_framework.views import APIView
+from app.models import Overview_serie
+from app.serializers import OverviewSerieSerializer
